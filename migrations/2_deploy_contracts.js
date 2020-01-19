978e90dbcfc8be8bbc97638f14dfab58ab37dcee
Ibo Sy <ibo@b9lab.com>
2020-01-19 11:21:20 +0100

    update deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,19 +2,20 @@ const Whitelist = artifacts.require("./Whitelist.sol");
 const EuroClaimTokenContract = artifacts.require("./EuroClaimTokenContract.sol");
 const BondsContract = artifacts.require("./BondsContract.sol");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
     let doc = "";
     let maxSupplyCap = 50000000;
     let minPurchaseAmount = 1000;
 
-    let wl = await deployer.deploy(Whitelist);
-
-    console.log("whitelist deployed:");
-    console.log(wl);
-    console.log(wl.address);
-
-    bonds = deployer.deploy(BondsContract, wl.address, doc, maxSupplyCap, minPurchaseAmount);
-    euroToken = deployer.deploy(EuroClaimTokenContract);
-
-    // Whitelist whitelist, uint256 document, uint256 maxSupplyCap, uint256 minPurchaseAmount
+    deployer.deploy(Whitelist)
+      .then(wl => {
+        console.log("deploy BondsContract");
+        return deployer.deploy(BondsContract, wl.address, doc, maxSupplyCap, minPurchaseAmount);
+      })
+      .then(bonds => {
+        console.log("deploy EuroToken");
+        return euroToken = deployer.deploy(EuroClaimTokenContract, bonds.address);
+      }).then(euroToken => {
+        console.log("euro token contract address: " + euroToken.address);
+      })
 };
