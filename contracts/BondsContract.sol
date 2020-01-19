7dc438d32d83355608c3c563544df83b1dd4ada8
Ibo Sy <ibo@b9lab.com>
2020-01-19 10:41:31 +0100

    update contract names

--- contracts/BondsContract.sol
+++ contracts/BondsContract.sol
@@ -2,8 +2,8 @@ pragma solidity 0.5.1;
 
 import "./SafeMath.sol";
 import "./ERC20Interface.sol";
-import "./EuroClaimToken.sol";
-import "./Whitelist.sol";
+import "./EuroClaimTokenContract.sol";
+import "./WhitelistContract.sol";
 import "./Ownable.sol";
 
 
@@ -29,6 +29,8 @@ contract BondsContract is ERC20Interface, Ownable {
     uint256 public _document;
 
     address private _claimTokenAddress;
+
+    bool public frozen;
 
     constructor (Whitelist whitelist, uint256 document, uint256 maxSupplyCap, uint256 minPurchaseAmount) public {
         _name = "Most Compliant and Regulated Token";
@@ -61,11 +63,6 @@ contract BondsContract is ERC20Interface, Ownable {
 
     /**
      * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
      */
     function transfer(address recipient, uint256 amount) public returns (bool) {
         _transfer(msg.sender, recipient, amount);
@@ -87,10 +84,6 @@ contract BondsContract is ERC20Interface, Ownable {
 
     /**
      * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
      */
     function approve(address spender, uint256 amount) public returns (bool) {
         _approve(msg.sender, spender, amount);
@@ -99,15 +92,6 @@ contract BondsContract is ERC20Interface, Ownable {
 
     /**
      * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20};
-     *
-     * Requirements:
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for `sender`'s tokens of at least
-     * `amount`.
      */
     function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
         _transfer(sender, recipient, amount);
@@ -115,60 +99,11 @@ contract BondsContract is ERC20Interface, Ownable {
         return true;
     }
 
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
-        _approve(msg.sender, spender, _allowances[msg.sender][spender].add(addedValue));
-        return true;
-    }
-
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
-    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
-        _approve(msg.sender, spender, _allowances[msg.sender][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
-        return true;
-    }
-
-    /**
-     * @dev Moves tokens `amount` from `sender` to `recipient`.
-     *
-     * This is internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     */
+   
     function _transfer(address sender, address recipient, uint256 amount) internal {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
-
+        require(!frozen);
         _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
         _balances[recipient] = _balances[recipient].add(amount);
         emit Transfer(sender, recipient, amount);
@@ -176,16 +111,10 @@ contract BondsContract is ERC20Interface, Ownable {
 
     /** @dev Creates `amount` tokens and assigns them to `account`, increasing
      * the total supply.
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `to` cannot be the zero address.
      */
     function _mint(address account, uint256 amount) internal {
         require(account != address(0), "ERC20: mint to the zero address");
-
+        require(!frozen);
         _totalSupply = _totalSupply.add(amount);
         _balances[account] = _balances[account].add(amount);
         require(_totalSupply <= _maxSupplyCap);
@@ -195,17 +124,10 @@ contract BondsContract is ERC20Interface, Ownable {
     /**
      * @dev Destroys `amount` tokens from `account`, reducing the
      * total supply.
-     *
-     * Emits a {Transfer} event with `to` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `account` cannot be the zero address.
-     * - `account` must have at least `amount` tokens.
      */
     function _burn(address account, uint256 amount) internal {
         require(account != address(0), "ERC20: burn from the zero address");
-
+        require(!frozen);
         _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
         _totalSupply = _totalSupply.sub(amount);
         emit Transfer(account, address(0), amount);
@@ -213,16 +135,6 @@ contract BondsContract is ERC20Interface, Ownable {
 
     /**
      * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
-     *
-     * This is internal function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
      */
     function _approve(address owner, address spender, uint256 amount) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
@@ -231,19 +143,6 @@ contract BondsContract is ERC20Interface, Ownable {
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
-     * from the caller's allowance.
-     *
-     * See {_burn} and {_approve}.
-     */
-    function _burnFrom(address account, uint256 amount) internal {
-        _burn(account, amount);
-        _approve(account, msg.sender, _allowances[account][msg.sender].sub(amount, "ERC20: burn amount exceeds allowance"));
-    }
-
-
 
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
@@ -258,8 +157,8 @@ contract BondsContract is ERC20Interface, Ownable {
 
     }
 
-    function freeze(string memory message, uint256 freeze_document) public {
-
+    function freeze(string memory message, uint256 freeze_document) public onlyOwner {
+        frozen=true;
     }
 
     function buyToken(uint256 amount) public {
